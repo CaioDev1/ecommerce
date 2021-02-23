a79014feff69b9c28210617083ed8a7ba9793549
root <root@Acer-Laptop.localdomain>
2021-02-23 19:35:36 -0300

    nineth commit

--- server.ts
+++ server.ts
@@ -14,6 +14,10 @@ app.get('/home', (req, res) => {
     res.render('home')
 })
 
+app.get('/catalog', (req, res) => {
+    res.render('catalog')
+})
+
 app.listen(3000, () => {
     console.log('Server iniciado na porta 3000')
 })
