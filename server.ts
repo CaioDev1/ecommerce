82014826ea4c7442a5057187117af5df96fcff20
root <root@Acer-Laptop.localdomain>
2021-02-23 19:44:02 -0300

    nineth commit

--- server.ts
+++ server.ts
@@ -8,10 +8,10 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('teste')
-})
-app.get('/home', (req, res) => {
     res.render('home')
+})
+app.get('/product', (req, res) => {
+    res.render('product')
 })
 
 app.get('/catalog', (req, res) => {
